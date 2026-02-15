189beabf380f2f701df4f4794f8ffbcc5e1ef377
Rasel Mahmud <raselm606@gmail.com>
2026-02-15 23:39:02 +0600

    api added coindesk

--- app/api/coindex/route.js
+++ app/api/coindex/route.js
@@ -11,7 +11,7 @@ export async function GET() {
 
   const url =
     "https://data-api.coindesk.com/news/v1/article/list" +
-    `?lang=EN&limit=15&categories=DIGITAL%20ASSET%20TREASURY,ETHFI&api_key=${encodeURIComponent(key)}`;
+    `?lang=EN&limit=15&source_ids=coindesk&categories=ADMINISTRATIVEEVENTS,DIGITALASSETTREASURY,ETH&api_key=${encodeURIComponent(key)}`;
 
   const res = await fetch(url, {
     next: { revalidate: 60 },
