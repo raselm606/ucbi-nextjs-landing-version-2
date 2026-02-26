35cb5756e31f84532ad95a0df65902bd883aaa3a
Rasel Mahmud <raselm606@gmail.com>
2026-02-26 17:32:29 +0600

    tag manager added

--- app/(frontend)/layout.js
+++ app/(frontend)/layout.js
@@ -1,5 +1,7 @@
 import BootstrapProvider from '@/components/layouts/BootstrapProvider';
- 
+import Script from "next/script";
+
+const GTM_ID = "GTM-K6G7W6MT";
 
 
 import tasaOrbiter from '@/lib/fonts/tasaOrbiter';
@@ -48,7 +50,7 @@ const ogImage = "/logo_ucbi.jpg";
     type: "website",
     images: [
       {
-        url: ogImage,          // absolute URL
+        url: ogImage, // absolute URL
         //width: 1200,
         //height: 630,
         alt: "ucbibanking.io",
@@ -82,7 +84,33 @@ const ogImage = "/logo_ucbi.jpg";
 export default async function RootLayout({ children } ) {
   return (
     <html lang="en">
+    <head>
+      {/* GTM script */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+    </head>
       <body className={`${InterSans.variable} ${tasaOrbiter.variable}`}>
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <BootstrapProvider> 
           {children} 
         </BootstrapProvider>
