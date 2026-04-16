f0ab6d18cc4f2d51216abd3d6f140779570ac908
Rasel Mahmud <raselm606@gmail.com>
2026-04-16 20:30:22 +0600

    footer link

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -98,7 +98,7 @@ const Footer = () => {
                 <ul>
                   <li><Link target="_blank" href="https://etherscan.io/token/0x2adba23cf1252de095aced801e758b369ec10426">Blockchain</Link></li>
                   <li><Link data-bs-toggle="modal"   href="#submit-a-request">Submit a Request</Link></li>
-                  <li><Link target="_blank" href="https://calendly.com/ucbibanking">Strategy Advisory</Link></li>
+                  <li><Link target="_blank" href="https://calendly.com/ucbibanking">Partnership Advisory</Link></li>
                   <li><Link target="_blank" href="https://calendly.com/ucbibanking">Token Launch Support</Link></li>
                 </ul>
               </div>
