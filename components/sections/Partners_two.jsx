fa14d2e92a47c64aace49cc28ba08880c884900e
Rasel Mahmud <raselm606@gmail.com>
2026-07-03 19:49:21 +0600

    partner-section

--- components/sections/Partners_two.jsx
+++ components/sections/Partners_two.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import Marquee from "react-fast-marquee";
 import logo10 from "../../public/images/partners/10.png";
-import logo12 from "../../public/images/partners/12.jpg";
+import logo12 from "../../public/images/partners/12.png";
 // import logo2 from "../../public/images/partners/2.jpg";
 // import logo3 from "../../public/images/partners/3.jpg";
 // import logo3 from "../../public/images/partners/3.jpg";
 import logo13 from "../../public/images/partners/13.png";
-import logo14 from "../../public/images/partners/14.jpg";
-import logo5 from "../../public/images/partners/5.jpg";
-import logo7 from "../../public/images/partners/7.jpg";
+import logo14 from "../../public/images/partners/14.png";
+import logo5 from "../../public/images/partners/5.png";
+import logo7 from "../../public/images/partners/7.png";
 import logo9 from "../../public/images/partners/9.png";
 import kraken from "../../public/images/partners/k.png";
 const Partners =  () => {
