c343044e2baba27542f272126277e9bf3a3c78ad
Rasel Mahmud <raselm606@gmail.com>
2026-07-06 14:24:42 +0600

    nes section

--- components/sections/Advancing.jsx
+++ components/sections/Advancing.jsx
@@ -1,4 +1,7 @@
-import { advanceData } from "@/lib/mock-data/advancing";
+import Image from "next/image"
+import ad1 from "../../public/images/ad1.png"
+import ad2 from "../../public/images/ad2.png"
+import ad3 from "../../public/images/ad3.png"
 const Advancing =  () => { 
 
   return (
@@ -8,20 +11,51 @@ const Advancing =  () => {
             <div className="row">
                 <div className="col-lg-4">
                     <div className="advancing_content">
-                        <h2 className="section_title_dark" >
-                           Advancing  <br />
-                          Through Structured Growth
+                        <span>Our Focus</span>
+                        <h2>
+                           Advancing 
+                          Through  <br /> Structured Growth
                         </h2>
+                        <p>We are committed to building a solid foundation and driving sustainable growth through a clear stategy strong partnerships and institutional excellence</p>
                     </div>   
                 </div>
 
                 <div className="offset-lg-2 col-lg-6">
-                    {advanceData.map((item, index) => (
-                    <div className="advancing_para" key={item.id}>
-                        <h5 >{item.title}</h5>
-                        <p > {item.desc}</p>
+
+                    <div className="adsec_area">
+                        <div className="img_ara">
+                            <Image src={ad1} alt="" />
+                        </div>
+
+                        <div className="advancing_para">
+                            <h5 >Treasury Management</h5>
+                            <p >Treasury management focuses on diversified capital allocation and liquidity strategies to optimize risk adjusted returns Our institutional frameworks and discipline driven approach ensure resilient performance and shareholder value creation in dynamic market conditions</p>
+                        </div>
+
                     </div>
-                    ))}
+                   
+                   <div className="adsec_area">
+                        <div className="img_ara">
+                            <Image src={ad2} alt="" />
+                        </div>
+                   
+                        <div className="advancing_para">
+                            <h5 >Strengthening Institutional</h5>
+                            <p >Strengthening the institutional framework focuses on governance excellence and operational consistency By building robust risk controls compliance culture and strategic alignment we ensure long term resilience stakeholder trust and sustainable value creation</p>
+                        </div>
+                    </div>
+
+                    <div className="adsec_area">
+                        <div className="img_ara">
+                            <Image src={ad3} alt="" />
+                        </div>
+                   
+                        <div className="advancing_para">
+                            <h5 >Partnerships Global Expansion</h5>
+                            <p >Global partnerships provide the scale to develop innovative solutions and expand our ecosystem We invest in strategic collaborations support sustainable real world growth and accelerate the adoption of blockchain solutions within a transformative framework</p>
+                        </div>
+                    </div>
+                  
                      
                 </div>
 
