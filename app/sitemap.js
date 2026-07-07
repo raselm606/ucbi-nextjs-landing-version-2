fb0eb6b56ca5f1215ff46468f2196c947d79f194
Rasel Mahmud <raselm606@gmail.com>
2026-07-07 21:45:58 +0600

    sitemap new updated

--- app/sitemap.js
+++ app/sitemap.js
@@ -18,23 +18,20 @@ export default function sitemap() {
       priority: 0.9,
     },
 
+    
+    {
+      url: "https://doc.ucbibanking.io/ucbi_whitepaper",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+
     {
       url: "https://doc.ucbibanking.io/ucbi_terms_condition",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
-    {
-      url: "https://doc.ucbibanking.io/ucbi_whitepaper",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://doc.ucbibanking.io/ucbi_coinmarketcap",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
-    },
+   
   ];
 }
